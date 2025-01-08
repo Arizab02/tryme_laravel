8ca719cd1bf28e19d64fb5024a8564ae45b1cdb5
Arizab02 <arizab002@gmail.com>
2025-01-08 09:52:10 +0700

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\check;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        check::factory(1000)->create();
 
         User::factory()->create([
             'name' => 'Test User',
